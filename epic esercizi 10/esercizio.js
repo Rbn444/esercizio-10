92ecbc5bf672bb478d7b5bb86c8d614655013e8c
Rbn444 <106075599+Rbn444@users.noreply.github.com>
2022-06-07 15:34:22 +0200

    Add files via upload

---
+++ epic esercizi 10/esercizio.js
@@ -0,0 +1,52 @@
+let nome = "Mirtilla"
+var posizione = "Cliente"
+const Pigreco = 3.14
+
+console.log(nome)
+console.log(posizione)
+console.log(Pigreco)
+
+nome = "Lilla"
+posizione = "Amministratrice"
+
+console.log(nome)
+console.log(posizione)
+
+let Lavoro1 = "developing";
+let Lavoro2 = "formazione";
+console.log("io mi occupo di: " +Lavoro1+ " e " +Lavoro2) 
+
+let professione = Lavoro1+ " e " +Lavoro2;
+console.log("io mi occupo di: " +professione)
+
+let boolean = true;
+console.log("Let JS: "+ boolean)
+
+let nonDefinito;
+console.log(nonDefinito);
+
+let anni = "30";
+console.log("Lilla ha: " +anni+ " anni")
+
+anni = 15;
+console.log(anni)
+
+anni -= 4
+console.log(anni)
+
+anni += 3
+console.log(anni)
+
+nomePet = "Billo"
+colore = "rosso"
+console.log("il nome del pet: " + nomePet + " e il suo colore è: " + colore)
+
+let animale = nomePet + " e il suo colore è: " + colore;
+console.log("il nome del pet: "+ animale)
+
+console.log("stringhe con\n righe \n multple")
+console.log("stringhe con\nrighe \nmultple")
+
+
+
+
